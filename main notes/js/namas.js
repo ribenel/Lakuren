a136245546d159cc113a9459f6554ca629618adb
ribenel <vlccpmm.111@gmail.com>
2026-01-18 18:36:08 -0300

    Add files via upload

---
+++ main notes/js/namas.js
@@ -0,0 +1,370 @@
+// alert("hola amoor")
+
+// let num, nume, numer, numero;
+
+// num = 10;
+// nume = 9;
+// numer = 8;
+// numero = 7; 
+
+// alert(num);
+// alert(nume);
+// alert(numer);
+// alert(numero);
+
+
+// let num = 2;
+// let num2 = "texto";
+
+// alert(num * num2);
+
+// prompt("hola bb")
+
+// let num = prompt("pon tu nombre amor :D");
+// alert("como que " + num + "?");
+
+
+// let num = 15;
+
+// num = 15 + 5;
+// num += 5; 
+
+// document.write(num)
+
+
+// let num = 10;
+// num **= 3;
+// document.write(num);
+
+
+// num1 = 2;
+// num2 = 5;
+// res = num1&num2;
+// alert(res);
+
+
+// num1 = 2;
+// res = -num1;
+// alert(res);
+
+
+
+// saludo = "hola amoor :) ";
+// pregunta = "¿como estas?";
+
+// frase = saludo + pregunta;
+
+// document.write(frase);
+
+
+// num1 = "10";
+// num2 = 20;
+
+// frase = num1.concat(num2);
+// document.write(frase);
+
+
+// nombre = "cosa";
+// frase = "si esta " + nombre + " me dice algo lo mato" ;
+// document.write(frase);
+
+// nombre = "cosa";
+// frase = `si esta ${nombre}  me dice algo lo mato`;
+// document.write(frase);
+
+
+// frase = `<div></div> * {cosa}`;
+// document.write(frase);
+
+
+
+// frase = 'hola "amor", ¿como estas?';
+// document.write(frase);
+// frase = `hola `amor`, ¿como estas?`;
+
+
+// val1 = 12;
+// val2 = 24;
+// val3 = 35;
+// val4 = 12;
+// val5 = 53;
+
+// resp = (val5 > val2 || val1 < val4) && (!(val3 != val1) || val4 != val2);
+
+// alert(resp);
+// document.write(res);
+
+
+
+// if (26 > 24) {
+// 	alert("hola");
+// }
+// if (24 != 14) {
+// 	alert("amor");
+// }
+
+
+
+// peso = 2400
+
+// if (peso == 24 ) {
+// 	alert("estabilidad economica ya que tienes mas de " + peso + "$");
+// }
+// else if (peso == 240 ) {
+// 	alert("estas en bancarrota ya que no tienes " + peso + "$");
+// }
+// else {
+// 	alert("no has informado sobre tus ingresos");
+// }
+
+
+// nombre = "pedro2";
+
+// if (nombre == "carlos") {
+// 	alert("hola " + nombre + " ¿como estas?");
+// }
+// else if (nombre == "pedro") {
+// 	alert("hola " + nombre + " te voy a llamar carlos");
+// }
+// else {
+// 	alert("no estas registrado");
+// }
+
+
+
+
+
+// dinCaren = prompt("¿cuanto dinero tenes Caren?");
+// dinCaren = parseInt(dinCaren);
+
+// if (dinCaren >= 5 && dinCaren < 20) {
+// 	alert("Caren deberia comparse el helado de agua");
+// 	alert("Te sobrarian " + (dinCaren - 5));
+// }
+// else if (dinCaren >= 20 && dinCaren < 130) {
+// 	alert("Caren deberia comparse el helado de crema de mani :)");
+// 	alert("Te sobrarian " + (dinCaren - 20));
+// }
+// else if (dinCaren >= 130 && dinCaren < 350) {
+// 	alert("Caren deberia comparse el helado de fresa :D");
+// 	alert("Te sobrarian " + (dinCaren - 130));
+// }
+// else if (dinCaren >= 350 && dinCaren < 900) {
+// 	alert("Caren deberia comparse el helado de vodka :D");
+// 	alert("Te sobrarian " + (dinCaren - 350));
+// }
+// else if (dinCaren >= 900 && dinCaren < 1000) {
+// 	alert("Caren deberia comparse el helado arandanos con polvo de oro o un kilo de helado");
+// 	alert("Te sobrarian " + (dinCaren - 900));
+// }
+// else {
+// 	alert("Caren, no jodas, sino viniste con plata largate de aca");
+// }
+
+
+// dinJonh = prompt("¿cuanto dinero tenes Jonh?");
+// dinJonh = parseInt(dinJonh);
+
+// if (dinJonh >= 5 && dinJonh < 20) {
+// 	alert("Jonh deberia comparse el helado de agua");
+// 	alert("Te sobrarian " + (dinJonh - 5));
+// }
+// else if (dinJonh >= 20 && dinJonh < 130) {
+// 	alert("Jonh deberia comparse el helado de crema de mani :)");
+// 	alert("Te sobrarian " + (dinJonh - 20));
+// }
+// else if (dinJonh >= 130 && dinJonh < 350) {
+// 	alert("Jonh deberia comparse el helado de fresa :D");
+// 	alert("Te sobrarian " + (dinJonh - 130));
+// }
+// else if (dinJonh >= 350 && dinJonh < 900) {
+// 	alert("Jonh deberia comparse el helado de vodka :D");
+// 	alert("Te sobrarian " + (dinJonh - 350));
+// }
+// else if (dinJonh >= 900 && dinJonh < 1000) {
+// 	alert("Jonh deberia comparse el helado arandanos con polvo de oro o un kilo de helado");
+// 	alert("Te sobrarian " + (dinJonh - 900));
+// }
+// else {
+// 	alert("Jonh, ¿de nuevo?, hoy salgo a las dos, esperame atras del basural y te dejo chuparmela a cambio del helado");
+// }
+
+// dinTailer = prompt("¿cuanto dinero tenes Tailer?");
+// dinTailer = parseInt(dinTailer);
+
+// if (dinTailer >= 5 && dinTailer < 20) {
+// 	alert("Tailer deberia comparse el helado de agua");
+// 	alert("Te sobrarian " + (dinTailer - 5));
+// }
+// else if (dinTailer >= 20 && dinTailer < 130) {
+// 	alert("Tailer deberia comparse el helado de crema de mani :)");
+// 	alert("Te sobrarian " + (dinTailer - 20));
+// }
+// else if (dinTailer >= 130 && dinTailer < 350) {
+// 	alert("Tailer deberia comparse el helado de fresa :D");
+// 	alert("Te sobrarian " + (dinTailer - 130));
+// }
+// else if (dinTailer >= 350 && dinTailer < 900) {
+// 	alert("Tailer deberia comparse el helado de vodka :D");
+// 	alert("Te sobrarian " + (dinTailer - 350));
+// }
+// else if (dinTailer >= 900 && dinTailer < 1000) {
+// 	alert("Tailer deberia comparse el helado arandanos con polvo de oro o un kilo de helado");
+// 	alert("Te sobrarian " + (dinTailer - 900));
+// }
+// else {
+// 	alert("Tailer, bienvenido, ¿como esta el presidente?, yo invito el helado :D");
+// }
+
+
+
+// let caja1 = {
+// 	cubo: "amarillo",
+// 	regla: "transparente",
+// 	palo: "marron",
+// 	angel: "rojo"
+// }
+// document.write(caja1["cubo"]); 
+
+
+// let caja2 = ["amarillo","transparente","marron","rojo"];
+// document.write(caja2[0]); 
+
+
+
+// let caja1 = {
+// 	cubo: "amarillo",
+// 	regla: "transparente",
+// 	palo: "balnco",
+// 	angel: "rojo"
+// }
+
+// let cubo = caja1["cubo"]
+// let regla = caja1["regla"]
+// let palo = caja1["palo"]
+// let angel = caja1["angel"]
+
+// frase = `Se ve como un cubo un tanto ${cubo}, 
+// 		pero aveces pienso que es mas una regla, por ese color tan ${regla} <br>
+// 		el otro dia vi un angel que era ${angel}, no imaginaba que los angeles fueran asi, <br>
+// 		luego desperte y tenia un palo ${palo}, clabado en el cuello, ¿habras sido el angel?`
+
+// document.write(frase);
+
+
+// let num = 15;
+
+// if (num < 19) {
+// 	num++;
+// 	alert(num)
+// }
+
+
+// let num = 0;
+
+// while (num < 10) {
+// 	num++;
+// 	document.write(num + `<br>`);
+// }
+
+
+
+// let num = 0;
+
+// do {
+// 	num++;
+// 	document.write(num + `<br>`);
+// }
+// while (num > 10)
+
+
+
+// let num = 0;
+
+// while(num < 100) {
+// 	num++;
+// 	document.write(num);
+// 	if (num == 15) {
+// 		break;
+// 	}
+// }
+
+// let i = 0;
+
+// for (let i = 3; i >= -9; i--) {
+// 	document.write(i + `<br>`);
+// }
+
+
+// for (let i = 0; i <= 10; i++) {
+// 	if (i == 8) {
+// 		continue;
+// 	}
+// 	document.write(i + `<br>`);
+// }
+
+// let arr1 = [" era "," una"," palabra"," mas"," que"];
+
+// let arr2 = ["kxg "," gdh"," pcx"," hola"," damm"];
+// hellooo:
+// for (arr in arr1) {
+// 	if (arr == 4) {
+// 		for (let arr of arr1) {
+// 			document.write(arr + `<br>`);
+// 		}
+// 		}
+// 		else {
+// 			document.write(arr2[arr]);
+// 	}
+// }
+
+function saludo(name) {
+	let frase = `¡hola ${name}!`
+	document.write(frase)
+}
+saludo("fff")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// intentate hacer ahora el siguiente algoritmo
+// un algoritmo que permita ingresar el salario de un operador
+// si el salario es menor de 100000, que calcule un aumento del 10% y lo muestre en pantalla
+// si el salario es mayor a 100000 y menor a 500000, que calcule un aumento del 8% y lo muestre en pantalla 
+// si el salario es MAYOR  a 500000, que no realice ningún aumento y se notifique que no corresponde uno
---
+++ main notes/practices/namas.js
@@ -0,0 +1,41 @@
+
+const num = 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// intentate hacer ahora el siguiente algoritmo
+// un algoritmo que permita ingresar el salario de un operador
+// si el salario es menor de 100000, que calcule un aumento del 10% y lo muestre en pantalla
+// si el salario es mayor a 100000 y menor a 500000, que calcule un aumento del 8% y lo muestre en pantalla 
+// si el salario es MAYOR  a 500000, que no realice ningún aumento y se notifique que no corresponde uno
